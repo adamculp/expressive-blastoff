46b3b6e0254863eab186c6a8a7e29c1607faa655
Matthew Weier O'Phinney <matthew@zend.com>
2017-03-09 16:59:09 -0600

    Removed obsolete dependencies

--- config/autoload/routes.global.php
+++ config/autoload/routes.global.php
@@ -1,42 +1,18 @@
 <?php
 
+use App\Action;
+use Zend\Expressive\Router;
+
 return [
     'dependencies' => [
         'invokables' => [
-            Zend\Expressive\Router\RouterInterface::class => 
-                Zend\Expressive\Router\FastRouteRouter::class,
-            App\Action\PingAction::class => App\Action\PingAction::class,
+            Router\RouterInterface::class => Router\FastRouteRouter::class,
+            Action\PingAction::class => Action\PingAction::class,
         ],
         'factories' => [
-            App\Action\HomePageAction::class => App\Action\HomePageFactory::class,
-            App\Action\UserListAction::class => App\Action\UserListFactory::class,
-            App\Action\UserDbalListAction::class => App\Action\UserDbalListFactory::class
-        ],
-    ],
-    'routes' => [
-        [
-            'name' => 'home',
-            'path' => '/',
-            'middleware' => App\Action\HomePageAction::class,
-            'allowed_methods' => ['GET'],
-        ],
-        [
-            'name' => 'api.ping',
-            'path' => '/api/ping',
-            'middleware' => App\Action\PingAction::class,
-            'allowed_methods' => ['GET'],
-        ],
-        [
-            'name' => 'user.list',
-            'path' => '/users',
-            'middleware' => App\Action\UserListAction::class,
-            'allowed_methods' => ['GET'],
-        ],
-        [
-            'name' => 'user.dbal.list',
-            'path' => '/users/dbal',
-            'middleware' => App\Action\UserDbalListAction::class,
-            'allowed_methods' => ['GET'],
+            Action\HomePageAction::class => Action\HomePageFactory::class,
+            Action\UserListAction::class => Action\UserListFactory::class,
+            Action\UserDbalListAction::class => Action\UserDbalListFactory::class
         ],
     ],
 ];
